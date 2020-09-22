f15d3a7927528b31596b66824b2568879c509efa
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2020-09-22 17:35:20 +0200

    adding chakra ui

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
+import { ThemeProvider, CSSReset, Box, Heading } from '@chakra-ui/core'
+
 const Home = () => (
-  <h1>Hi 👋 wir sind Kulturkram</h1>
+  <ThemeProvider>
+    <CSSReset />
+    <Heading p={8} fontSize={['xs', 'xl', '4xl']}>Hi 👋 wir sind Kulturkram</Heading>
+  </ThemeProvider>
 )
 
 export default Home
