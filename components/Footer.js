323fafd27cd6dade1417c5cd92369786ac77ddb1
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2020-09-23 07:22:38 +0200

    adding footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,37 @@
+import { Button, Box, Flex, Link } from '@chakra-ui/core'
+import NextLink from 'next/link'
+
 const Footer = ({ children }) => {
   return (
     <>
-      <p>Footer</p>
+      <Flex
+        flexDirection='row'
+        as='footer'
+        justifyContent='space-between'
+        alignItems='center'
+        maxWidth='900px'
+        width='100%'
+        mx='auto'
+        mt={[0, 8]}
+        p={8}
+
+      >
+        <Box>
+          Kulturkram e.V. 2020
+        </Box>
+        <Box>
+          <NextLink href='/' passHref>
+            <Link as='a' p={[1, 4]}>
+              Impressum
+            </Link>
+          </NextLink>
+          <NextLink href='/' passHref>
+            <Link as='a' p={[1, 4]}>
+              Datenschutzerklärung
+            </Link>
+          </NextLink>
+        </Box>
+      </Flex>
     </>
   )
 }
