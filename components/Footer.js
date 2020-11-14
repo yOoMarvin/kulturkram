447bfb3e02f1f6b54a2e9d454c088a5afb45f2f8
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2020-11-14 21:02:36 +0100

    coloring the footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,22 +1,39 @@
 import { Box, Flex, Link } from '@chakra-ui/core'
 import NextLink from 'next/link'
+import styled from '@emotion/styled'
 
+const ColoredFlex = styled(Flex)`
+z-index: 1;
+flex-direction: row;
+@media (max-width: 768px) {
+    flex-direction: column;
+  }
+&:before {
+  content:"";
+  background-color: #171923;
+  position: absolute;
+  height: 25%;
+  width: 200vw;
+  left: -100vw;
+  z-index: -1;
+}
+`
 const Footer = ({ children }) => {
   return (
     <>
-      <Flex
-        flexDirection='row'
+      <ColoredFlex
         as='footer'
         justifyContent='space-between'
         alignItems='center'
         maxWidth='900px'
+        bg='gray.900'
+        color='white'
         width='100%'
-        mx='auto'
         mt={[0, 8]}
         p={8}
 
       >
-        <Box>
+        <Box mb={[8, 0]}>
           Kulturkram e.V. 2020
         </Box>
         <Box>
@@ -25,13 +42,15 @@ const Footer = ({ children }) => {
               Impressum
             </Link>
           </NextLink>
+        </Box>
+        <Box>
           <NextLink href='/' passHref>
             <Link as='a' p={[1, 4]}>
               Datenschutzerklärung
             </Link>
           </NextLink>
         </Box>
-      </Flex>
+      </ColoredFlex>
     </>
   )
 }
